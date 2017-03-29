de9e8420beff0c84bc2d9b2fc18d43a7aceab926
Razvan Andrei Ionescu <ionescu77@gmail.com>
2017-03-29 16:19:25 +0300

    Fixed the logical error

--- app/src/main/java/com/example/android/practiceset2/MainActivity.java
+++ app/src/main/java/com/example/android/practiceset2/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         int weekend = 9;
         int optimalHours = 7 * 8;
 
-        int actualHours = weekday;
+        int actualHours = weekday * 5;
         actualHours = actualHours + weekend * 2;
         int solution = optimalHours - actualHours;
         display(solution);
